e0c828c106f793e4f3e2bfd351b0d4fd4bdf905c
Andrii Kanirovskyi <kanirovskyi@gmail.com>
2023-06-21 18:51:05 +0300

    task8.part3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const buttonSubmit = document.querySelector("button");
+
+// запись данных из инпутов в local storage
+form.addEventListener("input", throttle(() => {
+    localStorage.setItem("feedback-form-state", JSON.stringify({
+        email: emailInput.value,
+        message: messageInput.value,
+    }));
+    buttonSubmit.disabled = !(emailInput.value && messageInput.value);
+}, 500));
+
+//создание переменной, где будет получать данные из локального хранилища по ключу
+const saveData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+if (saveData) { 
+    emailInput.value = saveData.email;
+    messageInput.value = saveData.message;
+    buttonSubmit.disabled = !(emailInput.value && messageInput.value);
+}
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+
+    console.log({ email: emailInput.value, message: messageInput.value })
+    
+    form.reset()
+
+    localStorage.removeItem("feedback-form-state")
+
+    buttonSubmit.disabled = true;
+})
