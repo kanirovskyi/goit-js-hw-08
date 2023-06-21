94ba4591a40e7337a86a5a37e1851cfa36d5c640
Andrii Kanirovskyi <kanirovskyi@gmail.com>
2023-06-21 18:20:24 +0300

    task8.part2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+ 
+const videoPlayer = document.querySelector('#vimeo-player');
+
+// инициализировали плейер с элементом 
+const player = new Player(videoPlayer);
+
+// инициализация Throttle функции
+const timeThrottle = throttle((seconds) => {
+    // сохраняем время в локальное хранилище 
+    localStorage.setItem("videoplayer-current-time", seconds)
+}, 1000);
+
+// отслеживание события timeupdate с помощью метода on() 
+player.on('timeupdate', function(data) {
+   timeThrottle(data.seconds)
+});
+
+//создание переменной, где будет получать время из локального хранилища по ключу
+const saveTime = localStorage.getItem("videoplayer-current-time");
+
+if (saveTime) { 
+    player.setCurrentTime(saveTime)
+}
 
