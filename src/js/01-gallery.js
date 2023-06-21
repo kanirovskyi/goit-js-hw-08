0d21cf9fe74fef7d1b6a21aeb277a8442ffb25de
Andrii Kanirovskyi <kanirovskyi@gmail.com>
2023-06-21 17:35:47 +0300

    task8.part1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-// console.log(galleryItems);
+const imagesContainer = document.querySelector(".gallery");
+
+// функция создания карточки картинка в галерее
+function createImage(images) {
+    const imageItem = images.map(({ preview, original, description }) => `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+   `).join("");
+    
+    imagesContainer.insertAdjacentHTML("beforeend", imageItem);
+}
+
+//рендерится разметка
+createImage(galleryItems);
+
+// Инициализация plugin lightbox и добавление параметров
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
